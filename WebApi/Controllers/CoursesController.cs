d734e2751876828f0a548d8a226ec2793bb167fd
Eleonor <elidoescode@gmail.com>
2024-04-08 13:07:08 +0200

    Add project files.

---
+++ WebApi/Controllers/CoursesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CoursesController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetALl(int id)
+    {
+        return Ok();
+    }
+}
